fc3673f79e2d127f18aa45f0b99225a6e98e980f
ItsVanScoit <ianvanscoit02@gmail.com>
2026-06-11 16:26:55 -0600

    Add deleted files, and add all de homework of Segundo_parcial

--- 2ndo_Parcial/Proyecto_21/codigo21.h
+++
@@ -1,9 +0,0 @@
-#ifndef CODIGO21_H
-#define CODIGO21_H
-
-void quickSort(int arr[], int bajo, int alto);
-int particion(int arr[], int bajo, int alto);
-void mostrarArreglo(int arr[], int n);
-void llenarArreglo(int arr[], int& n);
-
-#endif
